13a24d452aeecba239310a2d8f18425841b2cdf5
msaaddev <mrsaadirfan@gmail.com>
2020-10-09 01:21:19 +0500

    📦 NEW: pod leader bio

--- components/Pod_Leader.js
+++ components/Pod_Leader.js
@@ -5,7 +5,16 @@ const Pod_Leader = () => {
     return (
         <div className={styles.pod_leader}>
             <h2>Meet our pod leader</h2>
-            <Card url='/images/arsalan.jpg' name='Arslan Khattak' pod='' />
+            <div className={styles.leader_description}>
+                <Card url='/images/arsalan.jpg' name='Arslan Khattak' pod='' />
+                <p>
+                    I'm a Computer Science Junior and a Full Stack (MERN) Developer and open source
+                    contributor. Being MLH'20 Fellow, I'm active Next.Js Contributor. Writing down
+                    what I have learned and sharing it with others is what I love to do. I love to
+                    write and speak about new technologies. I am also an active member of technical
+                    communities. I am DSC LEAD and Microsoft Student Partner.
+                </p>
+            </div>
         </div>
     );
 };
--- components/Seo.js
+++ components/Seo.js
@@ -5,6 +5,10 @@ const SEO = () => {
         <Head>
             <title>Coding Crow - MLH Pod 1.1.0</title>
             <link rel='icon' href='/images/logo.png' />
+            <link
+                href='https://fonts.googleapis.com/css2?family=Lora&display=swap'
+                rel='stylesheet'
+            />
         </Head>
     );
 };
