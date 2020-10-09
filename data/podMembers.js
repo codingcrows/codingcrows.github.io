51b4fc41ac38c19fb7e1db33f430fce224d9f89e
msaaddev <mrsaadirfan@gmail.com>
2020-10-10 01:10:33 +0500

    📦 NEW: bios

--- data/podMembers.js
+++ data/podMembers.js
@@ -3,70 +3,76 @@ module.exports = [
         name: 'Angelina Gasharova',
         url: '/images/Angelina_Gasharova.jpg',
         github: 'https://github.com/angelinag',
-        linkedin: 'https://linkedin.com/in/angelina-gasharova-6797691b7',
-        more: '/pod/angelina',
+        linkedin: 'https://www.linkedin.com/in/gasharova/',
+        bio: `I've been coding since I was 13. Many languages and frameworks later I've finally settled with Python as my all-time favourite; it's extremely powerful, yet so simple. This way, I don't have to waste so much effort - so that I can then happily waste it on other activities, such as ranking up a videogame or climbing mountain tops.`,
     },
     {
         name: 'Saad Irfan',
         url: '/images/saad.jpg',
         github: 'https://github.com/msaaddev',
         linkedin: 'https://linkedin.com/in/msaaddev',
-        more: '/pod/msaaddev',
+        bio: `Aspiring JavaScript developer getting a Bachelor's degree in Computer Sciences. Introduced to coding at age of 12. An MLH '20 fellow, a Jr. Dev Advocate, an open-sourcer (authored several open-source tools), open-source contributor (contributed to the likes of Node.js, Fluid Project, Global Public Inclusive Infrastructure), former Google's Developer Student Clubs Lead, Beta – Microsoft Learn Student Ambassador, Technical Writer, Speaker, TV Show fanatic, Wannabe Photographer and a Guitarist.`,
     },
     {
         name: 'Aneesh Kodali',
         url: '/images/Aneesh_Kodali.jpeg',
         github: 'https://github.com/aneeshkodali',
         linkedin: 'https://linkedin.com/in/aneeshkodali',
-        more: '/pod/aneeh',
+        bio: `Coming from data analytics background, I decided to pursue software engineering because I liked the idea of using code and logic and building things people can see and interact with. I like learning new languages (computer or otherwise). I enjoy playing tennis and eating, as I consider my day as just 'waiting until the next meal'`,
     },
     {
         name: 'Nkosilathi Tauro',
         url: '/images/Nkosilathi_Tauro.png',
         github: 'https://github.com/nkosi-tauro',
         linkedin: 'https://linkedin.com/in/nkosi-tauro',
-        more: '/pod/nkosilathi',
+        bio: `I solve problems, often using technology. I take pride in writing high quality code. I enjoy mentoring others, in-person and virtually. I can identify and solve problems, and have excellent communication skills which allow me to work well independently or as part of a team. I have expertise in web development, data science and game development. I enjoy coding in Python , but overall I'm not fussy about what languages or stacks I work with. I am good at learning new skills as I need them.`,
     },
     {
         name: 'Levy Naibei',
         url: '/images/levy.jpeg',
         github: 'https://github.com/Levy-Naibei',
         linkedin: 'https://linkedin.com/in/Levy-Naibei/',
-        more: '/pod/levy',
+        bio: `Passionate Full-stack JavaScript Developer | React.js, Node.js | Data Science and Cloud computing enthusiast | Hiking, enjoying nature, mentoring fellow young people |`,
     },
     {
         name: 'Anna Tselikova',
         url: '/images/ania.jpg',
         github: 'https://github.com/aniats',
         linkedin: 'https://linkedin.com/in/anna-tselikova-756496164/',
-        more: '/pod/anna',
+        bio: `20 y.o. Software Engineer from Russia; also in love with travelling, foreign languages and financial markets`,
     },
     {
         name: 'Egor Tarasov',
         url: '/images/egor.jpeg',
         github: 'https://github.com/Jorres',
         linkedin: 'https://linkedin.com/',
-        more: '/pod/egor',
+        bio: `20 y.o. software developer from Russia. Definitely not a competitive programmer.`,
     },
     {
         name: 'Arlyn Miles',
         url: '/images/arlyn.png',
         github: 'https://github.com/Acrylami',
         linkedin: 'https://www.linkedin.com/in/arlyn-m-06b4331b9/',
-        more: '/pod/arlyn',
+        bio: `Final year BSc Computer Science  student, Technocamps ambassador for Computer Science education in South Wales`,
     },
     {
         name: 'Osarumense Azamegbe',
         url: '/images/osas.jpg',
         github: 'https://github.com/david-osas',
         linkedin: 'https://www.linkedin.com/in/osarumense-azamegbe/',
-        more: '/pod/osarumense',
+        bio: `I am a Computer Engineering undergraduate and a full-stack developer using MERN technologies. I like creating awesome stuff with code, reading about new technological innovations, and discussing such with other people. I also like watching movies and anime, and I am also interested in other amazing fields of technology such as mobile development, machine learning, cloud computing, and robotics`,
     },
     {
         name: 'Yashika Sharma',
         url: '/images/yashika.jpg',
         github: 'https://github.com/yashika51',
         linkedin: 'https://www.linkedin.com/in/yashika51',
-        more: '/pod/yashika',
+        bio: `A soon to be graduated computer science engineer who loves to combine software engineering best practices with data science techniques to build impactful projects.
+I have been digging in the data science field for more than one year now. During my data science journey so far I worked as an ML Engineer at Omdena(worked on social good projects, because is it even tech if its isn't impactful?), data scientist at a startup called Mylo (I brought profits to company using NLP along with increasing the platform engagement by 2x) and as a MLH fellow where I spent amazing 12 weeks contributing to machine learning and deep learning organizations including scikit-learn, Keras and BentoML.
+
+Currently, I am working as a Junior Team Lead at MLH (because sharing my experience with others helps me to learn more and build a community), where I manage 10 brilliant fellows in US and Canada.
+
+Diversity matters a lot to me and to empower women in tech I lead Women Who Code Python track. They have some amazing study groups and AMA webinars going on that are led me.
+`,
     },
 ];
