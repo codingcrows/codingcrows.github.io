63d8477189b78ed641bce2e1fe5944fbf4d8fe0d
msaaddev <mrsaadirfan@gmail.com>
2020-10-10 02:29:30 +0500

    📦 NEW: meta tags

--- components/Seo.js
+++ components/Seo.js
@@ -9,6 +9,13 @@ const SEO = () => {
                 href='https://fonts.googleapis.com/css2?family=Lora&display=swap'
                 rel='stylesheet'
             />
+            <meta charset='UTF-8' />
+            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+            <meta name='description' content='MLH Fellowship Pod 1.1.0 Website' />
+            <meta
+                name='keywords'
+                content='MLH, MLH fellowship, MLH explorer, msaaddev, gasharova, aneeshkodali, nkosi-tauro, Levy-Naibei, aniats, Jorres, Acrylami, david-osas, yashika51'
+            />
         </Head>
     );
 };
